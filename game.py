44e67b4fca720ee06f07b9dbac196e9623a20b8f
Saimun Hassan <saimun4u@gmail.com>
2023-04-23 20:32:47 -0400

    Defined Humanplayer moves and numbers they can select

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ class TicTacToe:
     @staticmethod
     def print_board_nums():
         # Tells us 0 | 1 | 2 what number corresponds to what box
-        number_board = [[star[i] for i in range(j*3, (j+1)*3)] for j in range(3)]
+        number_board = [[str[i] for i in range(j*3, (j+1)*3)] for j in range(3)]
         for row in number_board:
            print('| ' + ' | '.join(row) + ' |')
 
