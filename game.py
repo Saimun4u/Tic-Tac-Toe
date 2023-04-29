bdb62923532fe4b9f597088c0e436f89de8727c2
Saimun Hassan <saimun4u@gmail.com>
2023-04-29 15:53:37 -0400

    Completed setting up the game module

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+from player import HumanPlayer, RandomComputerPlayer
+
 class TicTacToe:
     def __init__(self):
         self.board = [' ' for _ in range(9)]
@@ -99,3 +101,8 @@ def play(game, x_player, o_player, print_game=True):
         if print_game:
             print('It\'s a tie')
 
+if __name__ == '__main__':
+    x_player = Humanplayer('X')
+    o_player = RandomComputerPlayer('O')
+    t = TicTacToe()
+    play(t, x_player, o_player, print_game=True)
